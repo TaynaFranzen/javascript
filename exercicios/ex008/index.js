42fca98de7f070bfdc79a02718a9013ad4183d22
TaynaFranzen <123785285+TaynaFranzen@users.noreply.github.com>
2025-06-20 14:44:11 -0300

    sobre condições

---
+++ exercicios/ex008/index.js
@@ -0,0 +1,6 @@
+var vel = "12"
+console.log('a velocidade do meu carro é '+vel+'km/h')
+if( vel > 60){
+    console.log("você ultrapassou a velocidade permitida. MULTADO")
+}
+console.log("dirija sempre usando cinto de segurança")
