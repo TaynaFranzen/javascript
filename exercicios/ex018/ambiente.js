8e9f38da904d1e71d5e1caa335dedbbc7da7d603
TaynaFranzen <123785285+TaynaFranzen@users.noreply.github.com>
2025-07-29 03:02:29 -0300

    excercicios

---
+++ exercicios/ex018/ambiente.js
@@ -0,0 +1,40 @@
+  /*
+  let lid = [Michel , Jean , Gabriel]
+  lid.push( Gabriel )
+ let acio =  [Jaqueline , Guilherme , Andrei , Taina , bruna]
+ acio.push( Taina )
+ acio.push( bruna )
+ let aten = [ Samuel , Dionata , fabio ]
+ aten.push(Rafaela )
+ aten.push(kezia)
+ aten.push( Enilson )
+ console.log('Nossos atendentes sao ' + aten + '')
+ console.log('Nossos lideres sao ' + lid + '')
+ console.log('Nossos acionadores sao ' +acio+ '' )
+
+ let floripa = [Je , Beto , Goncalvez]
+ let itajai = [ LL  ]
+ let joinvile = [ marcelo ]
+ let criciuma = [ kako , bw  , embrasat ]
+ console.log('-----PRESTADORES-----')
+ 
+ console.log('Prestador Florianopolis ' + floripa + '')
+ console.log('Prestador de Itajai ' + itajai + '')
+ console.log('Prestador de Joivile ' + joinvile + '')
+ console.log('Prestador de Criciuma ' + criciuma + '' )  
+
+
+ let valores = [ 8 , 7 , 1 , 4 , 2 , 9]
+
+ console.log(valores)
+
+    for(let pos=0; pos < valores.length; pos++) {
+        console.log(` A posição ` + pos + 'tem o valor ' +valores[pos] + '')
+    }
+        */
+
+    for( let pos in num){
+            console.log(` A posição ` + pos + 'tem o valor ' +valores[pos] + '')
+    }
+
+    
